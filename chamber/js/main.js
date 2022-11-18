943c6094576e321df339174e65ceca3f860da0ef
Vitor Barros <vitormonteirodebarros@gmail.com>
2022-11-18 17:39:41 -0300

    w10

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -46,16 +46,6 @@ const checkCurrentMenuItem = () => {
   }
 }
 
-const checkBanner = () => {
-  const day = new Date().getDay();
-
-  // friday
-  if (day === 5) {
-    const banner = document.getElementById('banner');
-    banner.setAttribute('style', 'display: block')
-  }
-}
-
 const initObserver = () => {
   const images = document.querySelectorAll('[data-src]');
   const options = {
@@ -90,13 +80,50 @@ const initObserver = () => {
   });
 };
 
-(() => {
+const latitude = -22.000;
+const longitude = -49.000;
+const baseURL = 'https://api.openweathermap.org';
+const apiKey = '1d7bffed80c21a62856f00c87f60f8f5';
+
+const fetchWeather = async () => {
+  const url = `${baseURL}/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  const response = await fetch(url);
+
+  if (response.ok) {
+    return response.json();
+  }
+
+  return Promise.reject('error while trying to fetch the weather api');
+}
+
+const appendResult = ({ main, weather }) => {
+  const currentTemp = document.querySelector('#current-temp');
+  const weatherIcon = document.querySelector('#weather-icon');
+  const captionDesc = document.querySelector('figcaption');
+
+  currentTemp.innerHTML = `<p>${main.temp.toFixed(0)} F</p>`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weather[0].icon}.png`;
+  const desc = weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+}
+
+(async () => {
   const menu = document.getElementById('menu');
   if (menu) {
     menu.addEventListener('click', toggleClicked);
   }
 
   checkCurrentMenuItem();
-  checkBanner();
   initObserver();
+
+  try {
+    const weather = await fetchWeather();
+    appendResult(weather);
+  } catch (e) {
+    console.error('error while trying to fetch the weather api');
+  }
 })();
